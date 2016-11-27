93856f5397bdc8b2d831f35dc8d1adb58aaf7e8d
yjxiong <yjxiong@users.noreply.github.com>
2016-11-28 00:18:09 +0800

    fix after migrating personal site

--- models/get_reference_models.sh
+++ models/get_reference_models.sh
@@ -1,6 +1,6 @@
 # bn flow model was trained with cuDNN v4, we will use the weights upgraded to cuDNN v5
-wget -O models/bn_inception_anet_2016_temporal.caffemodel.v5 http://personal.ie.cuhk.edu.hk/~xy012/others/models/bn_inception_anet_2016_temporal.caffemodel.v5
+wget -O models/bn_inception_anet_2016_temporal.caffemodel.v5 http://yjxiong.me/others/models/bn_inception_anet_2016_temporal.caffemodel.v5
 
-wget -O models/bn_inception_anet_2016_temporal_deploy.prototxt http://personal.ie.cuhk.edu.hk/~xy012/others/models/bn_inception_anet_2016_temporal_deploy.prototxt
-wget -O models/resnet200_anet_2016.caffemodel http://personal.ie.cuhk.edu.hk/~xy012/others/models/resnet200_anet_2016.caffemodel
-wget -O models/resnet200_anet_2016_deploy.prototxt http://personal.ie.cuhk.edu.hk/~xy012/others/models/resnet200_anet_2016_deploy.prototxt
+wget -O models/bn_inception_anet_2016_temporal_deploy.prototxt http://yjxiong.me/others/models/bn_inception_anet_2016_temporal_deploy.prototxt
+wget -O models/resnet200_anet_2016.caffemodel http://yjxiong.me/others/models/resnet200_anet_2016.caffemodel
+wget -O models/resnet200_anet_2016_deploy.prototxt http://yjxiong.me/others/models/resnet200_anet_2016_deploy.prototxt
